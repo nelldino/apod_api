c9f4630d10597e6d78352fe1449219c3222850e8
Nelly <120444803+nelldino@users.noreply.github.com>
2025-05-18 19:59:28 +0300

    Swagger Documentation (#2)
    * add swagger documentation
    
    * test swagger implementation

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
-const express = require ('express');
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const app = express();
 const port = 3000;
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'APOD API',
+            version: '1.0.0',
+            description: 'API for Astronomy Picture of the Day (APOD)',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const routes = require('./routes/routes.js')(app, fs);
+require('./routes/routes.js')(app, fs);
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
